a49fe2b7b528d42db29028141cbf7d6ff3a9d2b8
aranaformae <fabienschmitz@hotmail.com>
2026-08-31 19:20:10 +0200

    Add central notifications menu

--- custom_components/puppy_tracker/config_flow.py
+++ custom_components/puppy_tracker/config_flow.py
@@ -37,6 +37,7 @@ class PuppyTrackerOptionsFlow(
                 "data_management",
                 "integrity",
                 "settings",
+                "notifications",
             ],
         )
 
